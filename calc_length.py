52c72e0e2e24e1efbc33aaaef7bd415e220fc750
Yklys Rodrigues <yklys10s@gmail.com>
2017-07-04 15:11:06 +0200

    plotting a graph and exporting it as pdf

--- calc_length.py
+++ calc_length.py
@@ -1,3 +1,5 @@
+import matplotlib.pyplot as plt
+
 gff_fh = open("GCF_000006945.1_ASM694v1_genomic.gff")
 gene_lengths = []
 
@@ -5,12 +7,15 @@ for line in gff_fh:
 	if not line.startswith("#"):
 		split_line = line.split()
 		
+		# separating genes from other stuff
 		if split_line[2]== "gene":
 			gene_start = int(split_line[3])
 			gene_end = int(split_line[4])
+			
 			cur_gene_length = gene_end - gene_start
-
-			#print("Gene start:",gene_start,"Gene end:",gene_end,"\nGene size: ",cur_gene_length)
 			gene_lengths.append(cur_gene_length)
 
-print(gene_lengths)
+#print(gene_lengths)
+#plotting length of genes
+plt.hist(gene_lengths)
+plt.savefig("gene_length_hist.pdf")
